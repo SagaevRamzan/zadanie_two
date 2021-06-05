5005c2d70c7071301e75a38dd80d8234842fcd23
SagaevRamzan <80630411+SagaevRamzan@users.noreply.github.com>
2021-06-05 17:26:54 +0300

    one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+
 
 function App() {
+    const [data, setData] = useState([]);
+    const [load, setLoad] = useState(true);
+    useEffect(() => {
+        fetch('http://jsonplaceholder.typicode.com/users')
+            .then((resp) => {
+                return resp.json()
+            })
+            .then((json) => {
+                setData(json)
+                setLoad(false);
+            })
+    }
+,[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="header my_bold">
+          <div className="name">name</div>
+          <div className="adr">city</div>
+          <div className="phone">phone</div>
+        </div>
+        <div>
+            {
+                (load?
+                    (<div>"идет загрузка"</div>):
+                data.map((item)=>{
+
+                    return (
+                        <div className="header">
+                            <div className="name">{item.name}</div>
+                            <div className="adr">{item.address.city}</div>
+                            <div className="phone">{item.phone}</div>
+                        </div>
+                    )
+                }))
+            }
+        </div>
     </div>
   );
 }
